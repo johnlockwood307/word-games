5aa21620b850c12bc0d0b503d4632d9e88507060
John Lockwood <johnlockwood307@gmail.com>
2025-09-12 12:27:16 -0400

    set up firestore for dictionary words

---
+++ api/util/FirebaseInit.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAB2n0ATGRsh9TK1rgb2swSWZBDGfGhOF8",
+  authDomain: "word-game-solvers.firebaseapp.com",
+  projectId: "word-game-solvers",
+  storageBucket: "word-game-solvers.firebasestorage.app",
+  messagingSenderId: "729595614987",
+  appId: "1:729595614987:web:d3db97d4ba66ec35e2740e",
+  measurementId: "G-9Y8KKXHYC7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
