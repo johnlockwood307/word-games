5aa529b9dd8d494301f4597b64364f0c0f123a8e
John Lockwood <johnlockwood307@gmail.com>
2025-10-07 01:51:58 -0400

    add serverside error log

--- app/api/util/firebaseAdmin.js
+++ app/api/util/firebaseAdmin.js
@@ -1,9 +1,14 @@
 import admin from "firebase-admin"
 
 if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY))
-    });
+    try {
+        admin.initializeApp({
+            credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY))
+        });
+    } catch (error) {
+        console.error("failed to initialize firebase admin sdk:", err);
+        throw err;
+    }
 }
 
 export const db = admin.firestore();
